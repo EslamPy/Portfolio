5397951dc959714350562f70a8808eb7fe6636d8
EslamPy <samdev@outlook.de>
2025-02-23 20:05:48 +0200

    test

--- src/Pages/Portofolio.jsx
+++ src/Pages/Portofolio.jsx
@@ -31,13 +31,7 @@ const ToggleButton = ({ onClick, isShowingMore }) => (
 
 function TabPanel({ children, value, index, ...other }) {
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
+    <div role="tabpanel" hidden={value !== index} id={`full-width-tabpanel-${index}`} aria-labelledby={`full-width-tab-${index}`} {...other}>
       {value === index && (
         <Box sx={{ p: { xs: 1, sm: 3 } }}>
           <Typography>{children}</Typography>
