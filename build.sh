064772b02bd211c6a4ffbdea84e3df33176502aa
Sam <132905586+EslamPy@users.noreply.github.com>
2025-04-20 15:43:26 +0200

    Fix esbuild dependency by installing missing linux module

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Install the missing Rollup dependency directly
+# Install the missing dependencies directly
 npm install --no-save @rollup/rollup-linux-x64-gnu
+npm install --no-save @esbuild/linux-x64
 # Then run the build
 node ./node_modules/vite/bin/vite.js build 
